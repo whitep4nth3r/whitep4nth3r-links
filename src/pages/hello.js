2ab9574d89ca3d9340cc5231c0246710705b1d1a
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-06-08 10:13:34 +0100

    :fire: Remove serverless routes from pages dir

--- src/pages/hello.js
+++
@@ -1,7 +0,0 @@
-// Enable the netlify adapter in astro.config.mjs to access this route at /hello
-
-export async function get() {
-  return new Response(JSON.stringify({ message: "Hello World from hello.js" }), {
-    status: 200,
-  });
-}
--- src/pages/hello.json.js
+++
@@ -1,7 +0,0 @@
-// Enable the netlify adapter in astro.config.mjs to access this route at /hello.json
-
-export async function get() {
-  return new Response(JSON.stringify({ message: "Hello World from hello.json" }), {
-    status: 200,
-  });
-}
